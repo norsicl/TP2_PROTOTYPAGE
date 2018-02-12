ea69a43b5849c930465a7b85313b938d8b14e96b
fbclol <boue.franck@orange.fr>
2018-02-12 17:18:15 +0100

    main

---
+++ src/m1/piu/ContactManagerFX.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class ContactManagerFX extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(ContactManagerFX.class, args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // soit on peut prendre le Interface_FB_LN.fxml
+        Parent root = FXMLLoader.load(getClass().getResource("view/FXMLFrame.fxml"));
+        Scene scene = new Scene(root, 1200, 720);
+        primaryStage.setTitle("Contact manager");
+        primaryStage.setScene(scene);
+
+        // pour récupérer un element de type Pane (conteneur)
+
+
+        primaryStage.show();
+
+
+        // pour afficher un message avant la fermeture de la fenetre
+        primaryStage.setOnCloseRequest(( WindowEvent we ) -> { System.out.println("Fermeture de l ' application");
+            primaryStage.close();
+        }) ;
+
+    }
+}
