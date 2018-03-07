2b764b8fb5ed41d47c85b55d50b86f0d4e47978f
fbclol <boue.franck@orange.fr>
2018-03-07 10:28:18 +0100

    frjio

--- src/m1/piu/Controller/ControllerMainFrame.java
+++ src/m1/piu/Controller/ControllerMainFrame.java
@@ -1,4 +1,30 @@
 package m1.piu.Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ControllerMainFrame {
+    public void openContactPanel(MouseEvent mouseEvent) {
+        Parent root = null;
+        Stage primaryStage = new Stage();
+        try {
+            root = FXMLLoader.load(getClass().getResource("../View/FXMLMainContactPanel.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Parent children = FXMLLoader.load(getClass().getResource("m1/piu/View/FXMLMainPanel.fxml"));
+        Scene scene = new Scene(root, 800, 400);
+
+        primaryStage.setTitle("Contact manager");
+        primaryStage.setScene(scene);
+        // pour récupérer un element de type Pane (conteneur)
+
+
+        primaryStage.show();
+    }
 }
--- src/m1/piu/Controller/ControllerTablePhones.java
+++ src/m1/piu/Controller/ControllerTablePhones.java
@@ -1,10 +1,34 @@
 package m1.piu.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ControllerTablePhones {
-    @FXML
-    public void openNewPhone(){
 
+    @FXML
+    public void openNewPhone(MouseEvent mouseEvent) {
+        Parent root = null;
+        Stage primaryStage = new Stage();
+        try {
+            root = FXMLLoader.load(getClass().getResource("../View/FXMLTablePhones.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Parent children = FXMLLoader.load(getClass().getResource("m1/piu/View/FXMLMainPanel.fxml"));
+        Scene scene = new Scene(root, 1200, 720);
+
+        primaryStage.setTitle("Contact manager");
+        primaryStage.setScene(scene);
+        // pour récupérer un element de type Pane (conteneur)
+
+
+        primaryStage.show();
     }
 }
