64a4f2cb8cfe1e800c40018fb768cd961164d04f
fbclol <boue.franck@orange.fr>
2018-03-12 08:47:24 +0100

    final

--- src/m1/piu/Controller/ControllerMainFrame.java
+++ src/m1/piu/Controller/ControllerMainFrame.java
@@ -1,18 +1,11 @@
 package m1.piu.Controller;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 
 public class ControllerMainFrame {
